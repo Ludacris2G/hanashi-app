a7987f9fdc41d05484827c2f5a276cbd7d0dc371
Liviu <liviutoma946@gmail.com>
2023-07-16 19:13:41 +0300

    fixed transition effect

--- client/src/components/ThemeButton.js
+++ client/src/components/ThemeButton.js
@@ -12,7 +12,7 @@ function ThemeButton() {
       type='button'
       className={`rounded-full w-14 h-8 flex items-center justify-${
         isDarkMode ? 'end' : 'start'
-      } bg-gray-300 dark:bg-gray-700 transition-transform duration-300 absolute`}
+      } bg-gray-300 dark:bg-gray-700 absolute z-10`}
       onClick={toggleDarkMode}
       style={{
         right: '1rem',
@@ -20,19 +20,15 @@ function ThemeButton() {
       }}
     >
       <span
-        className={`rounded-full w-6 h-6 bg-white dark:bg-gray-200 shadow-md transform transition-transform duration-300`}
+        className={`rounded-full w-6 h-6 bg-white dark:bg-gray-200 shadow-md transform `}
         style={{
           transform: `translate3d(${
             isDarkMode ? 'calc(100% + 4px)' : '4px'
           }, 0, 0)`,
           marginLeft: `${isDarkMode ? '-1px' : '0'}`,
+          transition: 'transform linear 200ms',
         }}
       />
-      <style jsx>{`
-        button:focus {
-          outline: none;
-        }
-      `}</style>
     </button>
   );
 }
