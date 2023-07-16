34b2fb88e451b34a1b0ef4ff47f12c6b9106c29f
Liviu <liviutoma946@gmail.com>
2023-07-16 15:07:53 +0300

    added app js

--- server/app.js
+++ server/app.js
@@ -1,3 +1,15 @@
 const express = require('express');
 const app = express();
 require('dotenv').config();
+
+const port = process.env.PORT | 5001;
+
+const start = async () => {
+  try {
+    app.listen(port, console.log(`Listening on port ${port}`));
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+start();
