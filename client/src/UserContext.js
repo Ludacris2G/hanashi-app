b87ca23f319bb2e78576cfadee4ffedad9c7ced6
Liviu <liviutoma946@gmail.com>
2023-07-25 20:39:02 +0300

    added use effect | fetchData

--- client/src/UserContext.js
+++ client/src/UserContext.js
@@ -1,10 +1,22 @@
-import { createContext, useState } from 'react';
+import axios from 'axios';
+import { createContext, useEffect, useState } from 'react';
 
 export const UserContext = createContext({});
 
 export function UserContextProvider({ children }) {
   const [username, setUsername] = useState(null);
   const [id, setId] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const token = await axios.get('/api/v1/auth/profile');
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
   return (
     <UserContext.Provider value={{ username, setUsername, id, setId }}>
       {children}
