12069639001805628eaba1eb7848084b83c579c2
Liviu <liviutoma946@gmail.com>
2023-07-26 20:58:21 +0300

    added react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,33 @@
 import axios from 'axios';
-import { UserContextProvider } from './UserContext';
-import Routes from './Routes';
+import { UserContext, UserContextProvider } from './UserContext';
+import Register from './Register';
+import { BrowserRouter, Redirect, Route, Routes } from 'react-router-dom';
+
+import { useContext, useEffect, useState } from 'react';
+import NotFound from './NotFound';
+import Chats from './Chats';
 
 function App() {
+  const [user, setUser] = useState(null);
+
   axios.defaults.baseURL = 'http://localhost:5001/';
   axios.defaults.withCredentials = true;
+
   return (
     <>
       <UserContextProvider>
-        <Routes />
+        <BrowserRouter>
+          <Routes>
+            {/* Public route: Register */}
+            <Route path='/' element={<Register setUser={setUser} />} />
+
+            {/* Private route: Chat */}
+            <Route path='/chats' element={<Chats user={user} />}></Route>
+
+            {/* Not Found Route */}
+            <Route path='*' element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
       </UserContextProvider>
     </>
   );
