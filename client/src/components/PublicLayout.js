65881003441bd3e76b1ca36977c12c26ace643a2
Liviu <liviutoma946@gmail.com>
2023-07-28 01:05:14 +0300

    added login | register layout

---
+++ client/src/components/PublicLayout.js
@@ -0,0 +1,45 @@
+import nightwind from 'nightwind/helper';
+import React, { useState } from 'react';
+import { Outlet } from 'react-router-dom';
+
+function PublicLayout() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+    nightwind.toggle();
+  };
+
+  return (
+    <>
+      <div className='bg-primary-200 dark:bg-primary-950 h-screen flex relative items-center'>
+        {/* THEME TOGGLE BUTTON */}
+        <button
+          type='button'
+          className={`rounded-full w-14 h-8 flex items-center justify-${
+            isDarkMode ? 'end' : 'start'
+          } bg-gray-300 dark:bg-gray-700 absolute z-10`}
+          onClick={toggleDarkMode}
+          style={{
+            right: '1rem',
+            top: '1rem',
+          }}
+        >
+          <span
+            className={`rounded-full w-6 h-6 bg-white dark:bg-gray-200 shadow-md transform `}
+            style={{
+              transform: `translate3d(${
+                isDarkMode ? 'calc(100% + 4px)' : '4px'
+              }, 0, 0)`,
+              marginLeft: `${isDarkMode ? '-1px' : '0'}`,
+              transition: 'transform linear 200ms',
+            }}
+          />
+        </button>
+        <Outlet />
+      </div>
+    </>
+  );
+}
+
+export default PublicLayout;
