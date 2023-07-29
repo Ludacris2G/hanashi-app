03c443459dae2d1c87fb345865b34a6e74762a4e
Liviu <liviutoma946@gmail.com>
2023-07-29 23:51:41 +0300

    added login router

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { register, verifyToken } = require('../controllers/auth');
+const { register, verifyToken, login } = require('../controllers/auth');
 
 router.post('/register', register);
 router.get('/profile', verifyToken);
+router.post('/login', login);
 
 module.exports = router;
