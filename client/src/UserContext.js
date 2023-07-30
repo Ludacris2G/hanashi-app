c31a61673c0abd61b2fbe5ca17eafc2f03e5d7d6
Liviu <liviutoma946@gmail.com>
2023-07-30 22:23:21 +0300

    added local storage validation

--- client/src/UserContext.js
+++ client/src/UserContext.js
@@ -7,16 +7,28 @@ export function UserContextProvider({ children }) {
   const [username, setUsername] = useState(null);
   const [id, setId] = useState(null);
   useEffect(() => {
-    const fetchData = async () => {
+    const token = localStorage.getItem('token');
+    const isValidToken = async (token) => {
       try {
-        const { data } = await axios.get('/api/v1/auth/profile');
-        setUsername(data.decoded.name);
-        setId(data.decoded.userId);
-      } catch (error) {}
+        axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+        const isValid = await axios.get('/api/v1/auth/profile');
+        setUsername('test');
+        setId('test1');
+      } catch (error) {
+        setUsername(null);
+        setId(null);
+        localStorage.removeItem('token');
+      }
     };
 
-    if (username) fetchData();
-  }, [username]);
+    if (token) {
+      isValidToken(token);
+    } else {
+      setUsername(null);
+      setId(null);
+    }
+  }, []);
+
   return (
     <UserContext.Provider value={{ username, setUsername, id, setId }}>
       {children}
