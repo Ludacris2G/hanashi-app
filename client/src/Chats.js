50507bd9dc2c5f969c4f76628c8c03ac0976a992
Liviu <liviutoma946@gmail.com>
2023-08-05 19:50:59 +0300

    added send message functionality

--- client/src/Chats.js
+++ client/src/Chats.js
@@ -6,6 +6,9 @@ function Chats() {
   const [ws, setWs] = useState(null);
   const [onlinePeople, setOnlinePeople] = useState({});
   const [selectedUserId, setSelectedUserId] = useState(null);
+  const [newMessageText, setNewMessageText] = useState('');
+  const [messages, setMessages] = useState([]);
+  const { username: user, id } = useContext(UserContext);
 
   useEffect(() => {
     const ws = new WebSocket('ws://localhost:5001');
@@ -15,21 +18,37 @@ function Chats() {
 
   function handleMessage(e) {
     const onlineUsers = JSON.parse(e.data);
-    if ('online' in onlineUsers) {
+    if (onlineUsers.online) {
       showOnlinePeople(onlineUsers.online);
+    } else {
+      const message = e.data;
     }
   }
 
   function showOnlinePeople(peopleArr) {
     const people = {};
     peopleArr.forEach(({ userId, username }) => {
-      people[userId] = username;
+      if (userId !== id) people[userId] = username;
     });
     setOnlinePeople(people);
   }
 
   function removePeopleHighlight(e) {
     if (e.currentTarget === e.target) setSelectedUserId(null);
+  }
+
+  function sendMessage(e) {
+    e.preventDefault();
+
+    ws.send(
+      JSON.stringify({
+        recipient: selectedUserId,
+        text: newMessageText,
+      })
+    );
+
+    setNewMessageText('');
+    setMessages((prev) => [...prev, { text: newMessageText, isOurs: true }]);
   }
 
   return (
@@ -42,14 +61,15 @@ function Chats() {
         <div className='text-center py-2 bg-primary-900 dark:bg-primary-50 border-b border-primary-900 font-bold tracking-wider'>
           HanashiApp 話
         </div>
-        {Object.keys(onlinePeople).map((userId, i) => (
+        {user}
+        {Object.keys(onlinePeople).map((userId) => (
           <div
             onClick={() => setSelectedUserId(userId)}
             className={
               ' border rounded-full border-primary-900 m-1 flex items-center gap-2 cursor-pointer ' +
-              (userId === selectedUserId ? 'bg-primary-500' : '')
+              (userId === selectedUserId ? 'bg-primary-600' : '')
             }
-            key={i}
+            key={userId}
           >
             <Avatar username={onlinePeople[userId]} userId={userId} />
             <span>{onlinePeople[userId]}</span>
@@ -58,27 +78,45 @@ function Chats() {
       </div>
       <div className='flex flex-col bg-primary-800 w-full p-2'>
         <div className='flex-grow text-primary-100'>
-          messages with selected person
+          {!selectedUserId && (
+            <div className='flex items-center justify-center h-full text-lg'>
+              &larr; please select a person
+            </div>
+          )}
+          {selectedUserId && (
+            <div>
+              {messages.map((message, i) => (
+                <div key={i}>{message.text}</div>
+              ))}
+            </div>
+          )}
         </div>
-        <div className='flex gap-2'>
-          <input
-            type='text'
-            className='bg-primary-100 border p-2 flex-grow rounded-sm w-0'
-            placeholder='Type here broseph'
-          />
-          <button className='bg-primary-900 p-2 text-primary-100 rounded-sm'>
-            <svg
-              viewBox='0 0 24 24'
-              fill='currentColor'
-              height='1em'
-              width='1em'
-              className='text-primary-50'
+        {selectedUserId && (
+          <form className='flex gap-2' onSubmit={sendMessage}>
+            <input
+              value={newMessageText}
+              onChange={(e) => setNewMessageText(e.target.value)}
+              type='text'
+              className='bg-primary-100 border p-2 flex-grow rounded-sm w-0'
+              placeholder='Type here broseph'
+            />
+            <button
+              type='submit'
+              className='bg-primary-900 p-2 text-primary-100 rounded-sm'
             >
-              <path fill='none' d='M0 0h24v24H0z' />
-              <path d='M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z' />
-            </svg>
-          </button>
-        </div>
+              <svg
+                viewBox='0 0 24 24'
+                fill='currentColor'
+                height='1em'
+                width='1em'
+                className='text-primary-50'
+              >
+                <path fill='none' d='M0 0h24v24H0z' />
+                <path d='M1.946 9.315c-.522-.174-.527-.455.01-.634l19.087-6.362c.529-.176.832.12.684.638l-5.454 19.086c-.15.529-.455.547-.679.045L12 14l6-8-8 6-8.054-2.685z' />
+              </svg>
+            </button>
+          </form>
+        )}
       </div>
     </div>
   );
