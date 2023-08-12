9dabac13eb7aa11a3f98104a2968bd79ae05d42e
Liviu <liviutoma946@gmail.com>
2023-08-12 22:13:50 +0300

    added online indicator

--- client/src/components/Avatar.js
+++ client/src/components/Avatar.js
@@ -1,13 +1,25 @@
 import React from 'react';
 
-function Avatar({ userId, username }) {
+function Avatar({ username, online }) {
   return (
     <div
-      className='bg-primary-800 rounded-full flex items-center'
+      className='bg-primary-800 relative rounded-full flex items-center'
       style={{ width: '35px', height: '35px' }}
     >
       <div className='text-center w-full text-primary-100 font-bold'>
         {username[0]}
+        {online && (
+          <div
+            className='absolute rounded-full border-primary-900 border'
+            style={{
+              height: '10px',
+              width: '10px',
+              right: '2px',
+              bottom: '1px',
+              backgroundColor: '#32fc39',
+            }}
+          ></div>
+        )}
       </div>
     </div>
   );
