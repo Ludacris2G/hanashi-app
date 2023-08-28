d67596c7c5d80f2f9c1bae4cb28e6e5bf8dd744e
Liviu <liviutoma946@gmail.com>
2023-08-28 21:17:21 +0300

    added logs in websocket.js

--- server/websocket/websocket.js
+++ server/websocket/websocket.js
@@ -26,120 +26,126 @@ const handleWebSocketConnection = (server) => {
   }
 
   wss.on('connection', (connection, req) => {
-    connection.isAlive = true;
-    console.log('connected at', new Date());
-    connection.timer = setInterval(() => {
-      connection.ping();
-      connection.deathTimer = setTimeout(() => {
-        clearInterval(connection.timer);
-        connection.isAlive = false;
-        connection.terminate();
+    try {
+      connection.isAlive = true;
+      console.log('connected at', new Date());
+      connection.timer = setInterval(() => {
+        connection.ping();
+        connection.deathTimer = setTimeout(() => {
+          clearInterval(connection.timer);
+          connection.isAlive = false;
+          connection.terminate();
+          sendOnlinePeople();
+        }, 1000);
+      }, 5000);
+
+      connection.on('pong', () => {
+        clearTimeout(connection.deathTimer);
+      });
+
+      connection.on('close', () => {
         sendOnlinePeople();
-      }, 1000);
-    }, 5000);
+        clearInterval(connection.timer);
+        clearTimeout(connection.deathTimer);
+      });
 
-    connection.on('pong', () => {
-      clearTimeout(connection.deathTimer);
-    });
-
-    connection.on('close', () => {
-      sendOnlinePeople();
-      clearInterval(connection.timer);
-      clearTimeout(connection.deathTimer);
-    });
-
-    // read username and id from the cookie
-    const { cookie } = req.headers;
-    if (cookie) {
-      const tokenCookieString = cookie
-        .split(';')
-        .find((str) => str.startsWith('token='));
-      const token = tokenCookieString.split('=')[1];
-      console.log('COOKIE: ', token);
-      if (token) {
-        try {
-          jwt.verify(token, process.env.JWT_SECRET, {}, (err, userData) => {
-            if (err) throw new UnauthenticatedError('Unauthorized');
-            const { userId, name } = userData;
-            connection.userId = userId;
-            connection.username = name;
-          });
-        } catch (error) {
-          console.log('logout true');
-          if (error instanceof UnauthenticatedError) {
-            connection.send(
-              JSON.stringify({
-                logout: true,
-              })
-            );
+      // read username and id from the cookie
+      const { cookie } = req.headers;
+      if (cookie) {
+        const tokenCookieString = cookie
+          .split(';')
+          .find((str) => str.startsWith('token='));
+        const token = tokenCookieString.split('=')[1];
+        console.log('COOKIE: ', token);
+        if (token) {
+          try {
+            jwt.verify(token, process.env.JWT_SECRET, {}, (err, userData) => {
+              if (err) throw new UnauthenticatedError('Unauthorized');
+              const { userId, name } = userData;
+              connection.userId = userId;
+              connection.username = name;
+            });
+          } catch (error) {
+            console.log('logout true');
+            if (error instanceof UnauthenticatedError) {
+              console.log('logged out from unauthenticated error catch');
+              connection.send(
+                JSON.stringify({
+                  logout: true,
+                })
+              );
+            }
           }
+        } else {
+          console.log('logged out from token check');
+          connection.send(
+            JSON.stringify({
+              logout: true,
+            })
+          );
         }
-      } else {
-        connection.send(
-          JSON.stringify({
-            logout: true,
-          })
-        );
       }
+
+      // send online people to all users
+      sendOnlinePeople();
+
+      connection.on('message', async (message) => {
+        if (!connection.userId) {
+          connection.send(
+            JSON.stringify({
+              logout: true,
+            })
+          );
+        }
+        const messageData = JSON.parse(message.toString());
+        const { recipient, text, file } = messageData;
+        let fileData = null;
+
+        if (file) {
+          const [, mimeType, base64WithoutPrefix] = file.data.match(
+            /^data:(.*);base64,(.*)$/
+          );
+
+          const bufferData = Buffer.from(base64WithoutPrefix, 'base64');
+          fileData = {
+            data: bufferData,
+            name: file.name,
+            mimeType: file.type,
+          };
+        }
+
+        if (recipient && text) {
+          console.log('sender id', connection.userId);
+          const messageDocData = {
+            sender: connection.userId,
+            recipient,
+            text: text,
+          };
+
+          if (fileData) {
+            messageDocData.file = fileData;
+          }
+
+          const messageDoc = await Message.create(messageDocData);
+
+          [...wss.clients]
+            .filter((client) => client.userId === recipient)
+            .forEach((client) => {
+              client.send(
+                JSON.stringify({
+                  text,
+                  sender: connection.userId,
+                  _id: messageDoc._id,
+                  timestamp: messageDoc.createdAt,
+                  recipient,
+                })
+              );
+            });
+        }
+      });
+    } catch (error) {
+      console.log('WebSocket Connection Error: ', error);
     }
-
-    // send online people to all users
-    sendOnlinePeople();
-
-    connection.on('message', async (message) => {
-      if (!connection.userId) {
-        connection.send(
-          JSON.stringify({
-            logout: true,
-          })
-        );
-      }
-      const messageData = JSON.parse(message.toString());
-      const { recipient, text, file } = messageData;
-      let fileData = null;
-
-      if (file) {
-        const [, mimeType, base64WithoutPrefix] = file.data.match(
-          /^data:(.*);base64,(.*)$/
-        );
-
-        const bufferData = Buffer.from(base64WithoutPrefix, 'base64');
-        fileData = {
-          data: bufferData,
-          name: file.name,
-          mimeType: file.type,
-        };
-      }
-
-      if (recipient && text) {
-        console.log('sender id', connection.userId);
-        const messageDocData = {
-          sender: connection.userId,
-          recipient,
-          text: text,
-        };
-
-        if (fileData) {
-          messageDocData.file = fileData;
-        }
-
-        const messageDoc = await Message.create(messageDocData);
-
-        [...wss.clients]
-          .filter((client) => client.userId === recipient)
-          .forEach((client) => {
-            client.send(
-              JSON.stringify({
-                text,
-                sender: connection.userId,
-                _id: messageDoc._id,
-                timestamp: messageDoc.createdAt,
-                recipient,
-              })
-            );
-          });
-      }
-    });
   });
 };
 
