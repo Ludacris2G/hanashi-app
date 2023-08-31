874a8452ed295d93a46ce7d4ae350e7a080435b9
Liviu <liviutoma946@gmail.com>
2023-08-31 23:00:00 +0300

    removed theme button

--- client/src/components/PublicLayout.js
+++ client/src/components/PublicLayout.js
@@ -3,39 +3,9 @@ import React, { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 
 function PublicLayout() {
-  const [isDarkMode, setIsDarkMode] = useState(false);
-
-  const toggleDarkMode = () => {
-    setIsDarkMode(!isDarkMode);
-    nightwind.toggle();
-  };
-
   return (
     <>
-      <div className='bg-primary-100 dark:bg-primary-950 h-screen flex relative items-center'>
-        {/* THEME TOGGLE BUTTON */}
-        <button
-          type='button'
-          className={`rounded-full w-14 h-8 flex items-center justify-${
-            isDarkMode ? 'end' : 'start'
-          } bg-gray-300 dark:bg-gray-700 absolute`}
-          onClick={toggleDarkMode}
-          style={{
-            right: '2.5rem',
-            top: '1rem',
-          }}
-        >
-          <span
-            className={`rounded-full w-6 h-6 bg-primary-800 dark:bg-gray-200 shadow-md transform `}
-            style={{
-              transform: `translate3d(${
-                isDarkMode ? 'calc(100% + 4px)' : '4px'
-              }, 0, 0)`,
-              marginLeft: `${isDarkMode ? '-1px' : '0'}`,
-              transition: 'transform linear 200ms',
-            }}
-          />
-        </button>
+      <div className='bg-primary-100 h-screen flex relative items-center'>
         <Outlet />
       </div>
     </>
