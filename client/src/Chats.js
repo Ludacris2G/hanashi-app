a9b638611e28524efd13eeb54613a8648ce4a941
Liviu <liviutoma946@gmail.com>
2023-09-02 19:35:07 +0300

    added menu toggle for small displays

--- client/src/Chats.js
+++ client/src/Chats.js
@@ -13,6 +13,7 @@ function Chats({ toggleDarkMode, isDarkMode }) {
   const [selectedUserId, setSelectedUserId] = useState(null);
   const [newMessageText, setNewMessageText] = useState('');
   const [messages, setMessages] = useState([]);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const { username: user, id, setUsername, setId } = useContext(UserContext);
   const navigate = useNavigate();
   const scrollReferenceDiv = useRef();
@@ -196,9 +197,30 @@ function Chats({ toggleDarkMode, isDarkMode }) {
 
   return (
     <div className='flex h-screen w-screen'>
+      <button
+        onClick={() => setIsMenuOpen(!isMenuOpen)}
+        className={`bg-primary-950 dark:bg-primary-900 ml-auto p-2 w-10 ${'block sm:hidden'}`}
+      >
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          fill='none'
+          viewBox='0 0 24 24'
+          strokeWidth={1.5}
+          stroke='currentColor'
+          className='w-6 h-6'
+        >
+          <path
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            d='M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.068.157 2.148.279 3.238.364.466.037.893.281 1.153.671L12 21l2.652-3.978c.26-.39.687-.634 1.153-.67 1.09-.086 2.17-.208 3.238-.365 1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z'
+          />
+        </svg>
+      </button>
       <div
-        className='bg-primary-950 w-1/3 text-primary-50 dark:text-primary-900 flex flex-col'
-        style={{ maxWidth: '300px' }}
+        className={`bg-primary-950 w-1/3 text-primary-50 dark:text-primary-900 flex flex-col ${`${
+          isMenuOpen ? 'block' : 'hidden sm:flex'
+        }`}`}
+        style={{ maxWidth: '300px', minWidth: '200px' }}
       >
         <div className='flex-grow' onClick={(e) => removePeopleHighlight(e)}>
           <div className='text-center py-2 bg-primary-900 dark:bg-primary-50 border-b border-primary-900 font-bold tracking-wider'>
