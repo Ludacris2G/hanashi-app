a596e8a467ccff6ee7af1211e5dbdb3630a5a969
Liviu <liviutoma946@gmail.com>
2023-09-02 14:59:54 +0300

    added logo

--- client/src/Register.js
+++ client/src/Register.js
@@ -46,38 +46,49 @@ function Register({ setUser, toggleDarkMode, isDarkMode }) {
         toggleDarkMode={toggleDarkMode}
         isDarkMode={isDarkMode}
       />
-      {/* REGISTER FORM */}
-      <form
-        style={{ width: '200px' }}
-        className='w-50 mx-auto mb-12'
-        onSubmit={register}
-      >
-        <input
-          className='block w-full rounded-sm p-2 mb-2 border'
-          type='text'
-          placeholder='username'
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <input
-          className='block w-full rounded-sm p-2 mb-2 border'
-          type='password'
-          placeholder='password'
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button className='bg-primary-700 text-primary-50  w-full rounded-sm p-2'>
-          Register
-        </button>
-        <p className='mt-1 text-center text-xs text-primary-900 font-thin'>
-          Already have an account?
-          <br />
-          <Link to='login' className='text-primary-500 font-normal'>
-            Log in here!
-          </Link>
+      <div className='w-50 mx-auto mb-12'>
+        {/* LOGO */}
+        <p
+          className='text-center mb-1 text-primary-100 font-semibold select-none'
+          style={{ fontSize: '90px' }}
+        >
+          話
         </p>
-        {error && <p style={{ color: 'red', textAlign: 'center' }}>{error}</p>}
-      </form>
+        <p className='text-center text-xl mb-5 text-primary-100 font-semibold select-none'>
+          HanashiApp
+        </p>
+
+        {/* REGISTER FORM */}
+        <form style={{ width: '200px' }} onSubmit={register}>
+          <input
+            className='block w-full rounded-sm p-2 mb-2 border'
+            type='text'
+            placeholder='username'
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <input
+            className='block w-full rounded-sm p-2 mb-2 border'
+            type='password'
+            placeholder='password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button className='bg-primary-200 text-primary-900  w-full rounded-sm p-2'>
+            Register
+          </button>
+          <p className='mt-1 text-center text-xs text-primary-100 font-thin'>
+            Already have an account?
+            <br />
+            <Link to='login' className='text-primary-500 font-normal'>
+              Log in here!
+            </Link>
+          </p>
+          {error && (
+            <p style={{ color: 'red', textAlign: 'center' }}>{error}</p>
+          )}
+        </form>
+      </div>
     </>
   );
 }
