1c33fc9ab2310e1211b7ad8a944799fe702b64a8
Liviu <liviutoma946@gmail.com>
2023-09-08 23:38:29 +0300

    added proper 404

--- client/src/NotFound.js
+++ client/src/NotFound.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; // You can add a link back to the homepage
 
 function NotFound() {
-  return <div>404 not found</div>;
+  return (
+    <div className='h-screen flex flex-col items-center justify-center bg-gradient-to-b from-blue-500 to-blue-800 text-white'>
+      <div className='text-6xl font-extrabold mb-4'>404</div>
+      <p className='text-xl mb-8'>Oops! Page not found</p>
+      <Link
+        to='/'
+        className='bg-primary-600 text-white py-2 px-4 rounded-full text-lg hover:bg-primary-700 transition duration-300'
+      >
+        Go back to homepage
+      </Link>
+    </div>
+  );
 }
 
 export default NotFound;
