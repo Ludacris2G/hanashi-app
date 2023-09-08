e6237713cd6437d3408953f70482a2fb9f703148
Liviu <liviutoma946@gmail.com>
2023-09-08 18:35:05 +0300

    added select none

--- client/src/components/People.js
+++ client/src/components/People.js
@@ -5,7 +5,7 @@ function People({ selectedUserId, onlinePeople, userId, online }) {
   return (
     <div
       className={
-        ' border rounded-full border-primary-900 m-1 flex items-center gap-2 cursor-pointer ' +
+        ' border rounded-full border-primary-900 m-1 flex items-center gap-2 cursor-pointer select-none ' +
         (userId === selectedUserId ? 'bg-primary-500' : '')
       }
       style={{ cursor: 'pointer' }}
