04f53fde13674fda6644955fc2612e39b5f12ed2
Liviu <liviutoma946@gmail.com>
2023-09-09 00:22:22 +0300

    added minor hover effect

--- client/src/components/ThemeButton.js
+++ client/src/components/ThemeButton.js
@@ -4,9 +4,9 @@ function ThemeButton({ toggleDarkMode, isDarkMode }) {
   return (
     <button
       type='button'
-      className={` rounded-full w-14 h-8 flex items-center justify-${
+      className={`rounded-full w-14 h-8 flex items-center justify-${
         isDarkMode ? 'end' : 'start'
-      } bg-primary-300 dark:bg-primary-800 `}
+      } bg-primary-300 dark:bg-primary-800`}
       onClick={toggleDarkMode}
       style={{
         backgroundColor: 'gray',
@@ -17,7 +17,7 @@ function ThemeButton({ toggleDarkMode, isDarkMode }) {
       }}
     >
       <span
-        className={`rounded-full w-6 h-6 bg-primary-800 dark:bg-primary-800 shadow-md transform `}
+        className={`hover:bg-primary-700 rounded-full w-6 h-6 bg-primary-800 dark:bg-primary-800 shadow-md transform `}
         style={{
           transform: `translate3d(${
             isDarkMode ? 'calc(100% + 4px)' : '4px'
