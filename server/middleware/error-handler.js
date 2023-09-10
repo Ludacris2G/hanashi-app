f70ec5f5f78d09a97ac8adccf7bfe009607aec09
Liviu <liviutoma946@gmail.com>
2023-09-10 20:48:25 +0300

    added username/password too short errors

--- server/middleware/error-handler.js
+++ server/middleware/error-handler.js
@@ -1,15 +1,37 @@
 const { StatusCodes } = require('http-status-codes');
 const errorHandlerMiddleware = (err, req, res, next) => {
   // default error
+  console.log(err.errors);
   let customError = {
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
     msg: err.message || 'Something went wrong, please try again later',
   };
 
+  // Check if the error is due to a duplicate key (e.g., user already exists)
   if (err.code && err.code === 11000) {
     customError.statusCode = StatusCodes.BAD_REQUEST;
     customError.msg = 'User already exists';
   }
+
+  // Check if the error is related to the 'password' field and its minimum length requirement
+  if (
+    err.errors?.password?.properties?.path === 'password' &&
+    err.errors?.password?.properties?.minlength
+  ) {
+    customError.msg = 'Your password should be at least 6 characters long.';
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+  }
+
+  // Check if the error is related to the 'username' field and its minimum length requirement
+  if (
+    err.errors?.username?.properties?.path === 'username' &&
+    err.errors?.username?.properties?.minlength
+  ) {
+    customError.msg = 'Your username should be at least 3 characters long.';
+    customError.statusCode = StatusCodes.BAD_REQUEST;
+  }
+
+  // Send the custom error response
   res.status(customError.statusCode).json({ msg: customError.msg });
 };
 
