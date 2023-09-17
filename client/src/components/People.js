57bed1e5d8bc9dcbce2690eba1a870c4380d8f49
Liviu <liviutoma946@gmail.com>
2023-09-17 16:26:38 +0300

    After requesting a conversation the app will wait for the data to be received before being able to make another request or unfocus the conversation

--- client/src/components/People.js
+++ client/src/components/People.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import Avatar from './Avatar';
 
-function People({ selectedUserId, onlinePeople, userId, online }) {
+function People({
+  selectedUserId,
+  onlinePeople,
+  userId,
+  online,
+  isConversationLoading,
+}) {
   return (
     <div
       className={
         'hover:bg-primary-400 transition duration-50 border rounded-full border-primary-900 m-1 flex items-center gap-2 cursor-pointer select-none ' +
         (userId === selectedUserId ? 'bg-primary-500' : '')
       }
-      style={{ cursor: 'pointer' }}
+      style={{ cursor: isConversationLoading ? 'not-allowed' : 'pointer' }}
       key={userId}
     >
       <Avatar online={online} username={onlinePeople[userId]} />
